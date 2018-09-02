a5ef7079d6cab6c715a77bde430f6d1d64fc373a
dkriskovic <dario.kriskovic@infobip.com>
2018-09-02 11:32:10 +0200

    add headers

--- handler.js
+++ handler.js
@@ -1,16 +1,27 @@
-export const hello = async (event, context, callback) => {
+import * as data from './mock/tournaments-data.json';
+
+export const tournamentsList = async (event, context, callback) => {
   const response = {
     statusCode: 200,
-    body: JSON.stringify({
-      message: `Go Serverless v1.0! ${(await message({ time: 1, copy: 'Your function executed successfully!'}))}`,
-    }),
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+    },
+    body: JSON.stringify(data.tournaments),
   };
 
   callback(null, response);
 };
 
-const message = ({ time, ...rest }) => new Promise((resolve, reject) => 
-  setTimeout(() => {
-    resolve(`${rest.copy} (with a delay)`);
-  }, time * 1000)
-);
+export const rounds = async (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+    },
+    body: JSON.stringify(data.rounds),
+  };
+
+  callback(null, response);
+};
